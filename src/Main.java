828bf8fa5066ab3face464798d6e42024254c3f5
jeann <jeann.rezende99@gmail.com>
2024-11-28 19:38:07 -0300

    Novas anotações

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,61 @@
 import model.Cachorro;
 import model.Gato;
 
+import java.util.ArrayList;
+
+/**
+ * Classe principal para estudo, contendo exemplos de manipulação de objetos, coleções, e controle de fluxo.
+ */
 public class Main {
     public static void main(String[] args) {
 
-        Gato gato1 = new Gato("Gutim",15);
+        // ===== Exemplo com objetos Gato =====
+        Gato gato1 = new Gato("Gutim", 15);
         gato1.fichaTecnica();
+
         Gato gato2 = new Gato("Zayon", 15);
         gato2.fichaTecnica();
         gato2.latir();
 
+        ArrayList<Gato> listaGato = new ArrayList<>();
+        listaGato.add(gato1);
+        listaGato.add(gato2);
+        System.out.println(listaGato.get(0).getNome());
+
+        // ===== Exemplo com objeto Cachorro =====
         Cachorro dog1 = new Cachorro("Tedy", 12);
         dog1.latir();
 
+        // ===== Exemplo de saída no console =====
         System.out.println("Olá Mundo!");
+
+        // ===== Exemplo com perfil =====
         Perfil p = new Perfil();
         p.setNome("Jeann");
         p.Saudacao();
+
+        // ===== Exemplo de operação de soma =====
         Soma s = new Soma();
-        s.Somar(10,10);
+        s.Somar(10, 10);
+
+        // ===== Exemplo de entrada e saída =====
         Input i = new Input();
         i.lerEscrever();
+
+        // ===== Exemplo de estruturas de repetição =====
         For f = new For();
         f.exemploFor();
+
         While w = new While();
         w.ExemploWhile(null);
+
+        // ===== Exemplo de estrutura condicional (switch) =====
         Switch sw = new Switch();
         sw.ExemploSwitch(1);
-        FunctionProcedure fp = new FunctionProcedure();
-        System.out.println("FunctionProcedure, Return: " + fp.somar(1,2));;
-        fp.exibirResultado(10);
 
+        // ===== Exemplo de funções e procedimentos =====
+        FunctionProcedure fp = new FunctionProcedure();
+        System.out.println("FunctionProcedure, Return: " + fp.somar(1, 2));
+        fp.exibirResultado(10);
     }
 }
--- src/model/AnimalHeranca.java
+++ src/model/AnimalHeranca.java
@@ -4,6 +4,21 @@ public class AnimalHeranca {
     private  String nome;
     private int  idade;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 
     public AnimalHeranca(String nome, int idade) {
         this.nome = nome;
@@ -14,9 +29,9 @@ public class AnimalHeranca {
         System.out.println("AUAU");
     }
 
-    public void fichaTecnica (){
-        System.out.println("Nome: "+nome);
-        System.out.println("Idade: "+idade);
+    public void fichaTecnica () {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
     }
 
 }
